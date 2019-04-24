15cb8b7c2e0b9f4c4a0b7db938101591b2a6b6ca
Tim <kindert1@tcnj.edu>
2019-04-24 14:21:22 -0400

    Added'with' statement for hdf5 file handling

--- store_population_data.py
+++ store_population_data.py
@@ -1,4 +1,5 @@
 #%%
+import h5py
 import os
 from pathlib import Path
 import time
@@ -12,16 +13,18 @@ files = [f for f in os.listdir(path) if f.endswith('.asc')]
 
 #%%
 #Reading the ASCII files takes some time, we will store their data in .npy files for faster access later
-for i in range(8):
-    print('Reading file {} (this could take a minute)...'.format(i+1))
-    t = time.time()
-    pane = np.loadtxt(Path(path, files[i]), dtype=np.float16, skiprows=6) #First 6 rows are header lines, skip them
-    elapsed = time.time() - t
-    
-    #Print a progress message to the user after the current file has been parsed
-    print('File {0} processed, took {1} seconds\n'.format(i+1, elapsed))
-    pane[(pane == -10000) | (pane == np.inf)] = 0
-    
-    #Save the numpy array in the same directory as the ASCII files
-    np.save(Path(path, 'pane{}.npy'.format(i+1)), pane)
+with h5py.File('GPW_Panes.h5', 'w') as f:
+    for i in range(8):
+        print('Reading file {} (this could take a minute)...'.format(i+1))
+        t = time.time()
+        pane = np.loadtxt(Path(path, files[i]), dtype=np.float16, skiprows=6) #First 6 rows are header lines, skip them
+        elapsed = time.time() - t
+        
+        #Print a progress message to the user after the current file has been parsed
+        print('File {0} processed, took {1} seconds\n'.format(i+1, elapsed))
+        pane[(pane == -10000) | (pane == np.inf)] = 0
+        
+        #Save the numpy array in the same directory as the ASCII files
+        f.create_dataset('pane{}'.format(i+1), data=pane)
+#        np.save(Path(path, 'pane{}.npy'.format(i+1)), pane)
 
